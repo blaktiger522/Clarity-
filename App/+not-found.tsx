408c2659d35ffe51f9b0f8738996aeda896c3e49
blaktiger522 <mahfoudhiahdi@gmail.com>
2025-07-18 12:29:32 +0100

    Create +not-found.tsx

---
+++ App/+not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function NotFound() {
+  return (
+    <View>
+      <Text>Page Not Found</Text>
+    </View>
+  );
+}
