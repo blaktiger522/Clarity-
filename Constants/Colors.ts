9ea80da15f3f699c0ab47c2a5760823d57695266
blaktiger522 <mahfoudhiahdi@gmail.com>
2025-07-21 10:10:16 +0100

    Create Colors.ts

---
+++ Constants/Colors.ts
@@ -0,0 +1,15 @@
+const colors = {
+  primary: '#2196F3', // Blue
+  secondary: '#90CAF9', // Light blue
+  background: '#F5F7FA',
+  text: '#1f2937',
+  lightGray: '#f3f4f6',
+  mediumGray: '#d1d5db',
+  darkGray: '#6b7280',
+  success: '#10b981',
+  danger: '#ef4444',
+  white: '#ffffff',
+  black: '#000000',
+};
+
+export default colors;
